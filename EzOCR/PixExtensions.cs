3a000b75384d873528a1f2650866b433f8460af1
Marcos Cordeiro <marcos.cordeiro@cedrotech.com>
2020-01-03 14:28:01 -0300

    Update PixExtensions.cs

--- EzOCR/PixExtensions.cs
+++ EzOCR/PixExtensions.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Threading.Tasks;
 using Tesseract;
 
 namespace EzOCR
@@ -37,6 +38,31 @@ namespace EzOCR
             writeStream.Close();
 
             return Pix.LoadFromFile(temporaryFile.FullPath);
+        }
+
+        /// <summary>
+        /// Ensures data folder exists, and run an OCR.
+        /// </summary>
+        /// <param name="pixImage">The image.</param>
+        /// <param name="dataPath">The directory holding the tesseract data.</param>
+        /// <param name="language">The language to use during the OCR process.</param>
+        /// <param name="disposeImage">If true, the image will be disposed after the OCR process is done.</param>
+        /// <returns></returns>
+        public static async Task<string> GetTextAndEnsureData(this Pix pixImage,
+                                                              string dataPath = "./tessdata",
+                                                              string language = "eng",
+                                                              bool disposeImage = true)
+        {
+            var tessdataDownloader = new TessdataDownloader();
+            await tessdataDownloader.EnsureDataFolder(language, dataFolder: dataPath);
+
+            using var engine = new TesseractEngine(dataPath, language, EngineMode.Default);
+            using var process = engine.Process(pixImage);
+            var text = process.GetText();
+
+            if (disposeImage) pixImage.Dispose();
+
+            return text;
         }
 
         /// <summary>
