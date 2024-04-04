171032bd867c6faf2ef5996edcd065a6fd72942b
Marcos Cordeiro <marcos.cordeiro@andbank-br.com.br>
2024-04-03 22:28:27 -0300

    Fixes potential memory leaks in TessdataDownloader

--- EzOCR/TessdataDownloader.cs
+++ EzOCR/TessdataDownloader.cs
@@ -8,33 +8,27 @@ namespace EzOCR
 {
     public class TessdataDownloader
     {
-        private GithubDownloader GithubDownloader { get; } = new GithubDownloader("tesseract-ocr", "tessdata");
+        private GithubDownloader GithubDownloader { get; } = new("tesseract-ocr", "tessdata");
 
         public async Task EnsureDataFolder(string language = "eng",
                                            string workingDirectory = null,
                                            string dataFolder = "tessdata")
         {
             var remoteFiles = await GithubDownloader.GetFiles(language);
-            var dataDirectory = new DirectoryInfo(Path.Combine(workingDirectory ?? Environment.CurrentDirectory,
-                    dataFolder ?? "tessdata"));
-            var localFiles = dataDirectory.Exists ? dataDirectory.GetFiles() : new FileInfo[0];
+            var dataDirectory = new DirectoryInfo(Path.Combine(workingDirectory ?? Environment.CurrentDirectory, dataFolder ?? "tessdata"));
+            var localFiles = dataDirectory.Exists ? dataDirectory.GetFiles() : [];
 
             if (!dataDirectory.Exists) dataDirectory.Create();
 
-            var client = new HttpClient();
+            using var client = new HttpClient();
 
-            foreach (var remoteFile in remoteFiles)
+            foreach (var remoteFile in remoteFiles.Where(remoteFile => localFiles.All(o => o.Name != remoteFile.Name)))
             {
-                if (localFiles.Any(o => o.Name == remoteFile.Name))
-                {
-                    continue;
-                }
-
-                var fileStream = await client.GetStreamAsync(remoteFile.DownloadUrl);
+                await using var fileStream = await client.GetStreamAsync(remoteFile.DownloadUrl);
                 var fileInfo = new FileInfo(Path.Combine(dataDirectory.FullName, remoteFile.Name));
 
-                using var localStream = fileInfo.OpenWrite();
-                fileStream.CopyTo(localStream);
+                await using var localStream = fileInfo.OpenWrite();
+                await fileStream.CopyToAsync(localStream);
             }
         }
     }
